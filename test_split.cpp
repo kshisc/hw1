a4c78443f24e56cf47128735951cac6694bf2184
Kyra Shi <kyrashi@usc.edu>
2024-01-29 01:07:41 +0000

    finished part 1

--- test_split.cpp
+++ test_split.cpp
@@ -11,7 +11,27 @@ g++ split.cpp test_split.cpp -o test_split
 
 #include "split.h"
 
+#include <iostream>
+using namespace std;
+
+void printList(Node* n){
+  while(n!=nullptr){
+    cout << n->value << " ";
+    n=n->next;
+  }
+}
+
 int main(int argc, char* argv[])
 {
+  Node* n3=new Node(3,nullptr);
+  Node* n2=new Node(2,n3);
+  Node* n1=new Node(1,n2);
+  Node* evens=nullptr;
+  Node* odds=nullptr;
 
+  split(n1,evens,odds);
+  printList(n1);
+  printList(evens);
+  printList(odds);
 }
+
